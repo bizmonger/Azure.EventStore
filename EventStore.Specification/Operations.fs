15b57ed456a5b3ebc5ae5b09a7a5c38e219de9cb
Bizmonger <scott.nimrod@bizmonger.net>
2020-11-05 21:39:04 -0500

    Removed unused function types

--- EventStore.Specification/Operations.fs
+++ EventStore.Specification/Operations.fs
@@ -1,12 +1,8 @@
 ﻿namespace EventStore
 
 open Language
-open EventStore.Core.Language
 
 module Operations =
 
     type Create    = ConnectionRequest -> AsyncResult<Connection , ErrorDescription>
     type Terminate = Connection        -> AsyncResult<unit       , ErrorDescription>
-
-    type AppendToStream           = Stream -> Event               -> ConnectionString -> AsyncResult<unit      , ErrorDescription>
-    type ReadStreamEventsBackward = Stream -> StartIndex -> Count -> ConnectionString -> AsyncResult<Event seq , ErrorDescription>
